d7e0c5e02deb6edd65f14cac158c0f05c0e93e08
Mikalai Kazlou <kazlou.mikalai.s@gmail.com>
2024-12-30 21:13:22 +0300

    feat: add some functions

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    let indexKey = 0;
+    let result = '';
+    const messageUp = message.toUpperCase();
+    const keyUpper = key.toUpperCase();
+
+    for (let index = 0; index < messageUp.length; index++) {
+      if (messageUp[index].match(/[A-Z]/)) {
+        const messageCharCode = messageUp.charCodeAt(index) - 'A'.charCodeAt(0);
+        const keyCharCode = keyUpper.charCodeAt(indexKey) - 'A'.charCodeAt(0);
+
+        const countPlus = (messageCharCode + keyCharCode) % this.alphabet.length;
+        result += this.alphabet[countPlus];
+
+
+        indexKey = (indexKey + 1) % keyUpper.length;
+      } else {
+        result += messageUp[index];
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
   }
+
+  decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+        let indexKey = 0;
+        let result = '';
+        const encryptedMessageUp = encryptedMessage.toUpperCase();
+        const keyUpper = key.toUpperCase();
+
+        for (let index = 0; index < encryptedMessageUp.length; index++) {
+            if (encryptedMessageUp[index].match(/[A-Z]/)) {
+                const encryptedCharCode = encryptedMessageUp.charCodeAt(index) - 'A'.charCodeAt(0);
+                const keyCharCode = keyUpper.charCodeAt(indexKey) - 'A'.charCodeAt(0);
+                const countMinus = (encryptedCharCode - keyCharCode + this.alphabet.length) % this.alphabet.length;
+                result += this.alphabet[countMinus];
+                indexKey = (indexKey + 1) % keyUpper.length;
+            } else {
+                result += encryptedMessageUp[index];
+            }
+        }
+
+        return this.direct ? result : result.split('').reverse().join('');
+    }
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+   if (!(date instanceof Date) || isNaN(date)) {
+        throw new Error('Invalid date!');
+    }
+    const month = date.getMonth();
+    const day = date.getDate();
+
+    if ((month === 11 && day >= 21) || month === 0 || month === 1 || (month === 2 && day < 20)) {
+        return 'winter';
+    } else if ((month === 2 && day >= 20) || month === 3 || month === 4 || (month === 5 && day < 21)) {
+        return 'spring';
+    } else if ((month === 5 && day >= 21) || month === 6 || month === 7 || (month === 8 && day < 23)) {
+        return 'summer';
+    } else {
+        return 'autumn';
+    }
 }
 
 module.exports = {
